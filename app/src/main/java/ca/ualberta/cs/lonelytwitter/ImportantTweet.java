1e6099450038ef5e5268dfcc1455d6ab38579374
aseniuk <aseniuk@ualberta.ca>
2017-09-14 19:05:34 -0600

    add some classes

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/ImportantTweet.java
@@ -0,0 +1,25 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+/**
+ * Created by aseniuk on 9/14/17.
+ */
+
+public class ImportantTweet extends Tweet {
+    public ImportantTweet(String message){
+        super(message);
+
+    }
+
+    public ImportantTweet(String message, Date date) {
+        super(message, date);
+    }
+
+    @Override
+    public Boolean isImportant() {
+        return Boolean.TRUE;
+    }
+
+
+}
